20f0cc1d6095ddadd3a2b543cfabee2f6f70bdb6
Patrice de Saint Steban <patrice@desaintsteban.fr>
2021-12-12 23:30:09 +0100

    Day 11

--- day11.js
+++ day11.js
@@ -1,17 +1,71 @@
 let test = [
-    ``,
-    ``
+    `5483143223
+    2745854711
+    5264556173
+    6141336146
+    6357385478
+    4167524645
+    2176841721
+    6882881134
+    4846848554
+    5283751526`,
+    `5433566276
+    6376253438
+    8458636316
+    6253254525
+    7211137138
+    1411526532
+    5788761424
+    8677841514
+    1622331631
+    5876712227`
 ];
 
-import {getTab, log} from './util.js';
+import {getTab, getTabLine, log} from './util.js';
 
-let tab = getTab(test)
+let tab = getTabLine(test).map(line => line.split('').map(n => parseInt(n)))
 
-log(tab);
+log(tab.map(line => line.join('')).join('\n'))
 
-function compute(tab) {
-    return 0;
+function incr(tab, i, j) {
+    if (i >= 0 && j >= 0 && i < tab.length && j < tab[i].length) {
+        //log(`${i}:${j} ${tab[i][j]}`)
+        tab[i][j]++;
+        if (tab[i][j] > 9) {
+            tab[i][j] = -10
+            let res = 1;
+            for (let k = -1; k <= 1; k++)
+                for (let l = -1; l <= 1; l++)
+                    res += incr(tab, i + k, j + l)
+            return res
+        }
+    }
+    return 0
 }
 
-console.log('Résultat 1: ', compute(tab));
+function compute(tab) {
+    let flash = 0;
+    for (let step = 1; step <= 1000; step++) {
+        log(`Step ${step}`)
+        for (let i = 0; i < tab.length; i++) {
+            for (let j = 0; j < tab[i].length; j++) {
+                flash += incr(tab, i, j)
+            }
+        }
+        let octopus = 0
+        for (let i = 0; i < tab.length; i++)
+            for (let j = 0; j < tab[i].length; j++)
+                if (tab[i][j] < 0) {
+                     tab[i][j] = 0;
+                     octopus++;
+                }
+        if (octopus === 100) {
+            console.log('Résultat 2: ', step)
+            break;
+        }
+        if (step <= 10) log(tab.map(line => line.join('')).join('\n'))
+        if (step === 100) console.log('Résultat 1: ', flash)
+    }
+}
 
+compute(tab)
